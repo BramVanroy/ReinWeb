590d5e0e385920b4c18d73b60ec1e6475d757a5a
Bram Vanroy <2779410+BramVanroy@users.noreply.github.com>
2024-09-16 12:29:36 +0200

    add yaml

--- scripts/reinweb.py
+++ scripts/reinweb.py
@@ -3,6 +3,11 @@ This file contains the code used to process and create the
 ReinWeb dataset. Heavily inspired by https://raw.githubusercontent.com/huggingface/datatrove/main/examples/fineweb.py
 """
 
+from pathlib import Path
+from typing import Literal
+
+import datatrove
+import yaml
 from datatrove.executor.slurm import SlurmPipelineExecutor
 from datatrove.pipeline.dedup import MinhashDedupCluster, MinhashDedupFilter, MinhashDedupSignature
 from datatrove.pipeline.dedup.minhash import MinhashConfig, MinhashDedupBuckets
@@ -21,6 +26,7 @@ from datatrove.pipeline.tokens import TokensCounter
 from datatrove.pipeline.writers.jsonl import JsonlWriter
 from datatrove.utils.hashing import HashConfig
 from datatrove.utils.typeshelper import Languages
+from pydantic import BaseModel
 
 from rein.utils import STOP_WORDS_DUTCH
 
@@ -43,18 +49,57 @@ new_line_ratio: float = 0.3, -> 0.264 (wiki: 99 percentile)
 """
 
 
-def main(
-    dump: str,
-    output_path: str,
-    partition: str,
-    download_extract_tasks: int = 8000,
-    minhash_tasks: int = 1000,
-    time_extractor: str = "10:00:00",
-    time_mh1: str = "5:00:00",
-    time_mh2: str = "02:00:00",
-    time_mh3: str = "30:00:00",
-    time_mh4: str = "5:00:00",
-):
+class BaseConfig(BaseModel):
+    tasks: int
+    time: str
+    mem_per_cpu_gb: int = 2
+    cpus_per_task: int = 1
+    randomize_start_duration: int = 0
+
+
+class ExtractorCfg(BaseConfig):
+    gopher_quality_filter: dict
+    c4_quality_filter: dict
+    fineweb_quality_filter: dict
+
+
+class HashCfg(BaseModel):
+    precision: Literal[32, 64] = 64
+    hash_fc: Literal["sha1", "xxhash"] = "sha1"
+
+
+class MinhashCfg(BaseModel):
+    hash_config: HashCfg
+    num_buckets: int
+    hashes_per_bucket: int
+    n_grams: int
+
+
+# Add Dutch policy substrings to the C4 filters
+datatrove.pipeline.filters.c4_filters.POLICY_SUBSTRINGS = set(
+    datatrove.pipeline.filters.c4_filters.POLICY_SUBSTRINGS
+    + [
+        "gebruik cookies",
+        "cookies aanvaarden",
+        "gebruik van cookies",
+        "cookies weigeren",
+        "gebruiksvoorwaarden",
+        "privacybeleid",
+        "cookiebeleid",
+    ]
+)
+
+
+def main(dump: str, output_path: str, partition: str, pipelines_config: str, venv_path: str | None = None):
+    configs = yaml.safe_load(Path(pipelines_config).read_text(encoding="utf-8"))
+
+    extract_cfg = ExtractorCfg(**configs["extractor"])
+    mh_cfg = MinhashCfg(**configs["minhash"])
+    mh1_cfg = BaseConfig(**configs["mh1"])
+    mh2_cfg = BaseConfig(**configs["mh2"])
+    mh3_cfg = BaseConfig(**configs["mh3"])
+    mh4_cfg = BaseConfig(**configs["mh4"])
+
     pd_base_filter = f"{output_path}/base_processing"
 
     main_processing_executor = SlurmPipelineExecutor(
@@ -67,40 +112,37 @@ def main(
             ),
             URLFilter(exclusion_writer=JsonlWriter(f"{pd_base_filter}/removed/1_url/{dump}")),
             Trafilatura(favour_precision=True),
-            LanguageFilter(languages="nld"),
+            LanguageFilter(languages="nld", language_threshold=0.8),
             GopherRepetitionFilter(
                 exclusion_writer=JsonlWriter(f"{pd_base_filter}/removed/3_gopher_rep/{dump}"), language=Languages.dutch
             ),
             GopherQualityFilter(
                 exclusion_writer=JsonlWriter(f"{pd_base_filter}/removed/4_gopher_qual/{dump}"),
-                max_avg_word_length=16,
-                max_non_alpha_words_ratio=0.806,
                 language=Languages.dutch,
                 stop_words=STOP_WORDS_DUTCH,
+                **extract_cfg.gopher_quality_filter,
             ),
             C4QualityFilter(
                 filter_no_terminal_punct=False,
                 exclusion_writer=JsonlWriter(f"{pd_base_filter}/removed/5_c4/{dump}"),
                 language=Languages.dutch,
+                **extract_cfg.c4_quality_filter,
             ),
             FineWebQualityFilter(
                 exclusion_writer=JsonlWriter(f"{pd_base_filter}/removed/6_fineweb_qual/{dump}"),
                 language=Languages.dutch,
-                line_punct_thr=0.088,
-                short_line_thr=0.67,
-                short_line_length=27,
-                char_duplicates_ratio=0.034,
-                new_line_ratio=0.264,
+                **extract_cfg.fineweb_quality_filter,
             ),
             JsonlWriter(f"{pd_base_filter}/output/{dump}"),
         ],
-        tasks=download_extract_tasks,
-        time=time_extractor,
+        tasks=extract_cfg.tasks,
+        time=extract_cfg.time,
         logging_dir=f"{output_path}/logs/base_processing/{dump}",
         slurm_logs_folder=f"reinweb-logs/base_processing/{dump}/slurm_logs",  # must be local
-        randomize_start_duration=180,  # don't hit the bucket all at once with the list requests
-        mem_per_cpu_gb=2,
+        randomize_start_duration=extract_cfg.randomize_start_duration,  # don't hit the bucket all at once with the list requests
+        mem_per_cpu_gb=extract_cfg.mem_per_cpu_gb,
         partition=partition,
+        venv_path=venv_path,
     )
     main_processing_executor.run()
 
@@ -111,12 +153,12 @@ def main(
     # you can also change ngrams or the number of buckets and their size here
     minhash_config = MinhashConfig(
         hash_config=HashConfig(
-            hash_fc="sha1",  # better precision -> fewer false positives (collisions)
-            precision=64,
+            hash_fc=mh_cfg.hash_config.hash_fc,  # better precision -> fewer false positives (collisions)
+            precision=mh_cfg.hash_config.precision,
         ),
-        num_buckets=14,
-        hashes_per_bucket=8,
-        n_grams=5,
+        num_buckets=mh_cfg.num_buckets,
+        hashes_per_bucket=mh_cfg.hashes_per_bucket,
+        n_grams=mh_cfg.n_grams,
     )
     pd_base_minhash = f"{output_path}/minhash"
     pd_logs_minhash = f"{output_path}/logs/minhash"
@@ -132,12 +174,13 @@ def main(
             INPUT_READER,
             MinhashDedupSignature(output_folder=f"{pd_base_minhash}/{dump}/signatures", config=minhash_config),
         ],
-        tasks=minhash_tasks,
-        time=time_mh1,
+        tasks=mh1_cfg.tasks,
+        time=mh1_cfg.time,
         partition=partition,
         logging_dir=f"{pd_logs_minhash}/signatures",
         slurm_logs_folder=f"{pd_slurm_logs_minhash}/signatures/slurm_logs",
-        randomize_start_duration=180,
+        randomize_start_duration=mh1_cfg.randomize_start_duration,
+        venv_path=venv_path,
         depends=main_processing_executor,  # only start after the first one completes
     )
 
@@ -152,12 +195,13 @@ def main(
         ],
         tasks=minhash_config.num_buckets * 50,  # the code supports parallelizing each bucket. here we run 50
         # workers per bucket
-        randomize_start_duration=180,
+        randomize_start_duration=mh2_cfg.randomize_start_duration,
         logging_dir=f"{pd_logs_minhash}/buckets",
         partition=partition,
-        time=time_mh2,
-        mem_per_cpu_gb=4,
-        cpus_per_task=3,  # you can add run more (smaller) tasks if you do not have a lot of memory
+        time=mh2_cfg.time,
+        mem_per_cpu_gb=mh2_cfg.mem_per_cpu_gb,
+        cpus_per_task=mh2_cfg.cpus_per_task,  # you can add run more (smaller) tasks if you do not have a lot of memory
+        venv_path=venv_path,
         depends=stage1,
     )
 
@@ -170,12 +214,13 @@ def main(
                 config=minhash_config,
             ),
         ],
-        tasks=1,  # this step runs on a single task
+        tasks=mh3_cfg.tasks,  # this step runs on a single task
         logging_dir=f"{pd_logs_minhash}/clustering",
         partition=partition,
-        time=time_mh3,  # and can also be quite slow. Usually not this slow though
-        mem_per_cpu_gb=25,
-        cpus_per_task=8,  # if you dedup a full dump, you do need a lot of memory for this one
+        time=mh3_cfg.time,  # and can also be quite slow. Usually not this slow though
+        mem_per_cpu_gb=mh3_cfg.mem_per_cpu_gb,
+        cpus_per_task=mh3_cfg.cpus_per_task,  # if you dedup a full dump, you do need a lot of memory for this one
+        venv_path=venv_path,
         depends=stage2,
     )
 
@@ -192,11 +237,12 @@ def main(
             PIIFormatter(),
             JsonlWriter(f"{pd_base_minhash}/{dump}/deduped_output"),
         ],
-        tasks=minhash_tasks,
+        tasks=mh1_cfg.tasks,
         logging_dir=f"{pd_logs_minhash}/filtering",
         partition=partition,
-        time=time_mh4,
-        mem_per_cpu_gb=4,
+        time=mh4_cfg.time,
+        mem_per_cpu_gb=mh4_cfg.mem_per_cpu_gb,
+        venv_path=venv_path,
         depends=stage3,
     )
 
@@ -212,15 +258,7 @@ if __name__ == "__main__":
         "--dump", type=str, required=True, help="CommonCrawl dump (see https://commoncrawl.org/overview)"
     )
     cparser.add_argument("--output_path", type=str, required=True, help="Output path")
-    cparser.add_argument(
-        "--download_extract_tasks", type=int, default=8000, help="Number of download and extract tasks"
-    )
-    cparser.add_argument("--minhash_tasks", type=int, default=1000, help="Number of minhash tasks")
-    cparser.add_argument("--partition", type=str, default="gpu", help="Slurm partition")
-    cparser.add_argument("--time_extractor", type=str, default="10:00:00", help="Time for the extractor")
-    cparser.add_argument("--time_mh1", type=str, default="5:00:00", help="Time for the first minhash stage")
-    cparser.add_argument("--time_mh2", type=str, default="02:00:00", help="Time for the second minhash stage")
-    cparser.add_argument("--time_mh3", type=str, default="30:00:00", help="Time for the third minhash stage")
-    cparser.add_argument("--time_mh4", type=str, default="5:00:00", help="Time for the fourth minhash stage")
+    cparser.add_argument("--pipelines_config", type=str, required=True, help="Path to the pipelines YAML config file")
+    cparser.add_argument("--venv_path", type=str, help="Path to the virtual environment")
     cli_kwargs = vars(cparser.parse_args())
     main(**cli_kwargs)
